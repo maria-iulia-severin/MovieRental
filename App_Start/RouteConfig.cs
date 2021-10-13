101424d23dbad73fb26177ba483064ba431449bf
Iulia Severin <iulia.severin@ulbsibiu.ro>
2021-10-13 09:26:33 +0300

    Models added

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,6 +13,21 @@ namespace MovieRental
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //varianta noua
+            routes.MapMvcAttributeRoutes();
+
+            //Acest cod e mai vechi si trebuie sa imi amintesc sa schimb fiecare chichita, dar pot face si cu Atribute Routing
+            //custom route added here conteaza ordinea in care le punem - de la most specific la most generic
+            //routes.MapRoute(
+                //"MoviesByReleaseDate",
+                //"movies/released/{year}/{month}",
+                //new { controller = "Movies", action = "ByReleaseDate" },
+                //acest argument foloseste un animus object si aplic regular expresion ca sa aplic niste constrangeri
+                // d vine de la digit si numarul inseamna numarul de repetii. Aron trebuie pus pt ca \ inseamna altceva singur.
+               // new { year = @"\d{4}", month = @"\d{2}" });
+                //costom routes.. adica aici pot sa am restrictie intre 2015 si 2016 sa fie anul
+                // new { year = @"\d{4}", month = @"\d{2}" });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieRental.Models;
+using MovieRental.ViewModels;
+
+namespace MovieRental.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Srak" };
+            var customers = new List<Customer>
+            { 
+                new Customer { Name = "Cust 1" },
+                new Customer { Name = "Cust 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            { 
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+        //movies - unde avem ? inseamna aca e optional
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+        //varianta mai usoara pt routing e sa adaug aici calea pe care o are, asa nu trebuie sa merg in routeconfig si sa modific 
+        //de fiecare data, am adaugat regex ca sa fie constrangerea
+        [Route("movies/released/{year}/{month:regex(\\d{4}:range(1,12)")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+    }
+}
