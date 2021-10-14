e99e7f379de2549027b3a167e93a8872242e666b
Iulia Severin <iulia.severin@ulbsibiu.ro>
2021-10-14 12:54:15 +0300

    Migration Added Database created succesfully

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -28,23 +28,25 @@ namespace MovieRental.Controllers
 
             return View(viewModel);
         }
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-        //movies - unde avem ? inseamna aca e optional
+        /*    public ActionResult Edit(int id)
+          {
+              return Content("id=" + id);
+          }
+          //movies - unde avem ? inseamna aca e optional
         public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
+          {
+              if (!pageIndex.HasValue)
+                  pageIndex = 1;
+              if (String.IsNullOrWhiteSpace(sortBy))
+                  sortBy = "Name";
+              return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+          }*/
+
         //varianta mai usoara pt routing e sa adaug aici calea pe care o are, asa nu trebuie sa merg in routeconfig si sa modific 
         //de fiecare data, am adaugat regex ca sa fie constrangerea
-        [Route("movies/released/{year}/{month:regex(\\d{4}:range(1,12)")]
-        public ActionResult ByReleaseDate(int year, int month)
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
         {
             return Content(year + "/" + month);
         }
