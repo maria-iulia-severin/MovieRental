059ddf14b831b149ecc4535a7d5b24739fdcaf17
Iulia Severin <iulia.severin@ulbsibiu.ro>
2021-11-21 19:05:54 +0200

    NavBar changes Movie Controller changes

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -10,46 +10,66 @@ namespace MovieRental.Controllers
 {
     public class MoviesController : Controller
     {
+        // GET: Movies/
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+
+        }
+
+        private object GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+
+        // ---------------
         // GET: Movies/Random
-        public ActionResult Random()
-        {
-            var movie = new Movie() { Name = "Srak" };
-            var customers = new List<Customer>
-            { 
-                new Customer { Name = "Cust 1" },
-                new Customer { Name = "Cust 2" }
-            };
+        /*        public ActionResult Random()
+                {
+                    var movie = new Movie() { Name = "Srak" };
+                    var customers = new List<Customer>
+                    { 
+                        new Customer { Name = "Cust 1" },
+                        new Customer { Name = "Cust 2" }
+                    };
 
-            var viewModel = new RandomMovieViewModel
-            { 
-                Movie = movie,
-                Customers = customers
-            };
+                    var viewModel = new RandomMovieViewModel
+                    { 
+                        Movie = movie,
+                        Customers = customers
+                    };
 
-            return View(viewModel);
-        }
-        /*    public ActionResult Edit(int id)
-          {
-              return Content("id=" + id);
-          }
-          //movies - unde avem ? inseamna aca e optional
-        public ActionResult Index(int? pageIndex, string sortBy)
-          {
-              if (!pageIndex.HasValue)
-                  pageIndex = 1;
-              if (String.IsNullOrWhiteSpace(sortBy))
-                  sortBy = "Name";
-              return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-          }*/
+                    return View(viewModel);
+                }
+                *//*    public ActionResult Edit(int id)
+                  {
+                      return Content("id=" + id);
+                  }
+                  //movies - unde avem ? inseamna aca e optional
+                public ActionResult Index(int? pageIndex, string sortBy)
+                  {
+                      if (!pageIndex.HasValue)
+                          pageIndex = 1;
+                      if (String.IsNullOrWhiteSpace(sortBy))
+                          sortBy = "Name";
+                      return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+                  }*//*
 
-        //varianta mai usoara pt routing e sa adaug aici calea pe care o are, asa nu trebuie sa merg in routeconfig si sa modific 
-        //de fiecare data, am adaugat regex ca sa fie constrangerea
+                //varianta mai usoara pt routing e sa adaug aici calea pe care o are, asa nu trebuie sa merg in routeconfig si sa modific 
+                //de fiecare data, am adaugat regex ca sa fie constrangerea
 
-        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
-        public ActionResult ByReleaseYear(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
+                [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+                public ActionResult ByReleaseYear(int year, int month)
+                {
+                    return Content(year + "/" + month);
+                }*/
 
     }
 }
