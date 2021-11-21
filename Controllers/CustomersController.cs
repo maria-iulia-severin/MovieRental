9d7398d1dcce9d37f71e5e71a731a1d5c7f2c117
Iulia Severin <iulia.severin@ulbsibiu.ro>
2021-11-21 18:53:14 +0200

    Customer Controller added

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MovieRental.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,35 @@ namespace MovieRental.Controllers
 {
     public class CustomersController : Controller
     {
-        // GET: Customers
-        public ActionResult Random()
+        //avem nevoie de DbContext ca sa putem folosi baza de date
+
+        private ApplicationDbContext _context;
+        public CustomersController()
         {
-            return View();
+            _context = new ApplicationDbContext();
+        }
+        //ApplicationDbContext este un disposable object si trebuie sa scapam de el corect
+        //adica override metoda Dispose a clasei Base
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Customers
+        public ActionResult Index()
+        {
+            //prin asta eu am acces la tot DbSet-ul Customers din baza de date
+            //acest query este de fapt executat doar cand mergem cu foreach prin tot dbSet-ul, adica in View-ul pt Customer
+            //var customers = _context.Customers;
+            //daca as vrea sa execut query-ul aici, as putea sa fac: 
+            var customers = _context.Customers.ToList();
+            return View(customers);
+        }
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+            return View(customer);
         }
     }
 }
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -9,6 +9,8 @@ namespace MovieRental.Models
     public class Customer
     {
         public int Id { get; set; }
+        /*Overriding Default Conventions se numeste Data Annotations și am folosit în modelul Customer.cs unde 
+         am constrans proprietatea Name sa not nullable și cu o dimensiune maxima de 255 de caractere.*/
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
