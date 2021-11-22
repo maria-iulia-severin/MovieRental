1e3a4d384f3131497fd7ad2586b45b3999543aa6
Iulia Severin <iulia.severin@ulbsibiu.ro>
2021-11-22 09:43:43 +0200

    Customer Form added for Edit or New Customer

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -9,14 +9,21 @@ namespace MovieRental.Models
     public class Customer
     {
         public int Id { get; set; }
+
         /*Overriding Default Conventions se numeste Data Annotations și am folosit în modelul Customer.cs unde 
          am constrans proprietatea Name sa not nullable și cu o dimensiune maxima de 255 de caractere.*/
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
+
         public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
+
+        [Display(Name = "Date of Birth")]
         public DateTime? Birthdate { get; set; }
     }
 }
---
+++ ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MovieRental.Models;
+
+namespace MovieRental.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        //IEnumerable is like a list, your getting a list of Membership Types (beacuse you need them all for the DropDown List)
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+
+    }
+}
